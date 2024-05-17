2f8a49799a5b928f0a98c1f4690a8cd4b0c3a647
Aquil Sheikh <aquil.she7kh@gmail.com>
2024-05-17 04:48:11 -0400

    Added new processing scripts and hInfinityFilter

--- src/utils/filterRun.py
+++
@@ -1,5 +0,0 @@
-from filter.filterbase import FilterBase
-from filter.adaptiveDenoiseKF import AdaptiveDenoiseKF
-from filter.denoiseKF import DenoiseKF
-def run_filter(data : list, filter : filter.FilterBase):
-    pass
---
+++ src/utils/processing.py
@@ -0,0 +1,50 @@
+from filter.filterbase import FilterBase
+from filter.adaptiveDenoiseKF import AdaptiveDenoiseKF
+from filter.denoiseKF import DenoiseKF
+from filter.hInfinityFilter import HInfinityFilter
+
+from numpy import array, mean
+
+def run_filter(data : array, filter : FilterBase):
+    """
+    Runs filter on data
+    Args:
+        data (list): measurements
+        filter (FilterBase): filter being used
+
+    Returns:
+        ret (list float): a list of filtered data
+    """
+    
+    ret = []
+    for item in list:
+        filter.kalman_iteration(data)
+        ret.append(filter.state)
+        
+    return array(ret)
+
+def calculate_error(data : array, filtered_data : array):
+    """
+    Calculates error at each data point
+    Args:
+        data (array): numpy array containing input data
+        filtered_data (array): numpy array containing filtered data
+
+    Returns:
+        (array): numpy array of error at each datapoint
+    """
+    
+    assert(len(filtered_data) == len(data))
+    return data - filtered_data
+    
+def calculate_rmse(error : array):
+    """
+    Calculates root mean squared error from error time series
+    Args:
+        error (array): error time series
+
+    Returns:
+        (float) : root mean square error
+    """
+    
+    return pow( mean( pow(error, 2) ) , 0.5)
